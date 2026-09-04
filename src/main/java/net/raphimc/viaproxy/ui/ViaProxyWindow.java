20f5b869d2057844a4b516b85bd34d6f747849ec
Your Name <you@example.com>
2026-09-04 15:56:14 +0200

    restore UI from 1.1.0: 500x380 arc12, BORDER 10, no pure black

--- src/main/java/net/raphimc/viaproxy/ui/ViaProxyWindow.java
+++ src/main/java/net/raphimc/viaproxy/ui/ViaProxyWindow.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of ViaProxy - https://github.com/RaphiMC/ViaProxy
+ * Copyright (C) 2021-2026 RK_01/RaphiMC and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.raphimc.viaproxy.ui;
 
 import com.formdev.flatlaf.FlatDarkLaf;
@@ -11,6 +28,7 @@ import net.raphimc.viaproxy.ViaProxy;
 import net.raphimc.viaproxy.ui.events.UICloseEvent;
 import net.raphimc.viaproxy.ui.impl.*;
 import net.raphimc.viaproxy.util.logging.Logger;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
@@ -20,21 +38,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ViaProxyWindow extends JFrame {
+
     public final LambdaManager eventManager = LambdaManager.threadSafe(new LambdaMetaFactoryGenerator(JavaBypass.TRUSTED_LOOKUP));
+
     public static final int BORDER_PADDING = 10;
     public static final int BODY_BLOCK_PADDING = 10;
+
     public final JTabbedPane contentPane = new JTabbedPane();
     private final List<UITab> tabs = new ArrayList<>();
+
     public final GeneralTab generalTab = new GeneralTab(this);
     public final AdvancedTab advancedTab = new AdvancedTab(this);
     public final AccountsTab accountsTab = new AccountsTab(this);
     public final UISettingsTab uiSettingsTab = new UISettingsTab(this);
+
     public ViaProxyWindow() {
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> showException(e));
         this.eventManager.register(this);
+
         this.setLookAndFeel();
         this.initWindow();
         this.initTabs();
+
         FlatInspector.install("ctrl shift I");
         FlatUIDefaultsInspector.install("ctrl shift O");
         ToolTipManager.sharedInstance().setInitialDelay(100);
@@ -42,20 +67,25 @@ public class ViaProxyWindow extends JFrame {
         SwingUtilities.updateComponentTreeUI(this);
         this.setVisible(true);
     }
+
     private void setLookAndFeel() {
         try {
             FlatDarkLaf.setup();
-            UIManager.put("Panel.background", new Color(30, 30, 30));
-            UIManager.put("TabbedPane.contentAreaColor", new Color(30, 30, 30));
-            UIManager.getLookAndFeelDefaults().put("TextComponent.arc", 5);
-            UIManager.getLookAndFeelDefaults().put("Button.arc", 5);
-        } catch (Throwable t) { t.printStackTrace(); }
+            UIManager.put("TextComponent.arc", 12);
+            UIManager.put("Button.arc", 12);
+            UIManager.put("Component.arc", 12);
+            UIManager.put("TabbedPane.showTabSeparators", true);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
+
     private void initWindow() {
         this.setTitle("FRM Proxy v" + ViaProxy.VERSION);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.addWindowListener(new WindowAdapter() {
-            @Override public void windowClosing(WindowEvent e) {
+            @Override
+            public void windowClosing(WindowEvent e) {
                 ViaProxyWindow.this.eventManager.call(new UICloseEvent());
                 ViaProxy.getConfig().save();
                 ViaProxy.getSaveManager().save();
@@ -66,33 +96,58 @@ public class ViaProxyWindow extends JFrame {
         this.setLocationRelativeTo(null);
         this.setContentPane(this.contentPane);
     }
+
     private void initTabs() {
-        RStream.of(this).fields().filter(field -> UITab.class.isAssignableFrom(field.type())).forEach(field -> {
-            final UITab tab = field.get();
-            this.tabs.add(field.get());
-            tab.add(this.contentPane);
-            this.eventManager.register(tab);
-        });
+        RStream
+                .of(this)
+                .fields()
+                .filter(field -> UITab.class.isAssignableFrom(field.type()))
+                .forEach(field -> {
+                    final UITab tab = field.get();
+                    this.tabs.add(field.get());
+                    tab.add(this.contentPane);
+                    this.eventManager.register(tab);
+                });
         this.contentPane.addChangeListener(e -> {
-            int idx = contentPane.getSelectedIndex();
-            if (idx >= 0 && idx < ViaProxyWindow.this.tabs.size()) ViaProxyWindow.this.tabs.get(idx).onTabOpened();
+            int selectedIndex = contentPane.getSelectedIndex();
+            if (selectedIndex >= 0 && selectedIndex < ViaProxyWindow.this.tabs.size()) ViaProxyWindow.this.tabs.get(selectedIndex).onTabOpened();
         });
     }
+
     public static void openURL(final String url) {
         try {
-            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) Desktop.getDesktop().browse(new URI(url));
-            else new ProcessBuilder("xdg-open", url).start();
-        } catch (Throwable t) { showInfo(I18n.get("generic.could_not_open_url", url)); }
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI(url));
+            } else {
+                new ProcessBuilder("xdg-open", url).start();
+            }
+        } catch (Throwable t) {
+            showInfo(I18n.get("generic.could_not_open_url", url));
+        }
     }
+
     public static void showException(final Throwable t) {
         Logger.LOGGER.error("Caught exception in thread " + Thread.currentThread().getName(), t);
-        StringBuilder b = new StringBuilder("An error occurred:\n");
-        b.append("[").append(t.getClass().getSimpleName()).append("] ").append(t.getMessage()).append("\n");
-        for (StackTraceElement e : t.getStackTrace()) b.append(e.toString()).append("\n");
-        showError(b.toString());
+        StringBuilder builder = new StringBuilder("An error occurred:\n");
+        builder.append("[").append(t.getClass().getSimpleName()).append("] ").append(t.getMessage()).append("\n");
+        for (StackTraceElement element : t.getStackTrace()) builder.append(element.toString()).append("\n");
+        showError(builder.toString());
     }
-    public static void showInfo(final String m){ showNotification(m, JOptionPane.INFORMATION_MESSAGE); }
-    public static void showWarning(final String m){ showNotification(m, JOptionPane.WARNING_MESSAGE); }
-    public static void showError(final String m){ showNotification(m, JOptionPane.ERROR_MESSAGE); }
-    public static void showNotification(final String message, final int type){ JOptionPane.showMessageDialog(ViaProxy.getForegroundWindow(), message, "FRM Proxy", type); }
+
+    public static void showInfo(final String message) {
+        showNotification(message, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void showWarning(final String message) {
+        showNotification(message, JOptionPane.WARNING_MESSAGE);
+    }
+
+    public static void showError(final String message) {
+        showNotification(message, JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void showNotification(final String message, final int type) {
+        JOptionPane.showMessageDialog(ViaProxy.getForegroundWindow(), message, "FRM Proxy", type);
+    }
+
 }
